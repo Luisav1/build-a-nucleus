69af730aca389bb6c39d3cba63e832ce1807d1ef
Luisav1 <52978048+Luisav1@users.noreply.github.com>
2020-10-21 13:09:33 -0600

    Add files via upload

---
+++ perennial/js/build-server/deployImages.js
@@ -0,0 +1,112 @@
+// Copyright 2020, University of Colorado Boulder
+
+const constants = require( './constants' );
+const execute = require( '../common/execute' );
+const fs = require( 'fs' );
+const gitCheckout = require( '../common/gitCheckout' );
+const gitPull = require( '../common/gitPull' );
+const request = require( 'request' );
+
+const chipperDir = '../chipper';
+
+/**
+ * This task deploys all image assets from the master branch to the latest version of all published sims.
+ *
+ * @param options
+ */
+const deployImages = async options => {
+  console.log( `deploying images with branch ${options.branch}, brands ${options.brands}` );
+  if ( options.branch ) {
+    await gitCheckout( 'chipper', options.branch );
+    await gitPull( 'chipper' );
+    await execute( 'npm', [ 'prune' ], chipperDir );
+    await execute( 'npm', [ 'update' ], chipperDir );
+  }
+
+  return new Promise( ( resolve, reject ) => {
+    try {
+      const url = `https://phet.colorado.edu/services/metadata/1.2/simulations?format=json&summary&locale=en&type=html${options.simulation ? `&simulation=${options.simulation}` : ''}`;
+
+      // Get all published sims
+      request( url, async function ( error, response, body ) {
+        if ( error ) {
+          console.error( 'failed to fetch metadata request', error );
+          reject( error );
+        }
+        else if ( response.statusCode < 200 || response.statusCode > 299 ) {
+          console.error( 'Bad Status while fetching metadata', response.statusCode );
+          reject( 'Bad Status while fetching metadata' );
+        }
+        else {
+          let projects;
+          try {
+            projects = JSON.parse( body ).projects;
+          }
+          catch ( e ) {
+            console.error( 'failed to parse json from metadata request', e );
+            reject( e );
+            return;
+          }
+
+          // Use for index loop to allow async/await
+          for ( let projectIndex = 0; projectIndex < projects.length; projectIndex++ ) {
+            const project = projects[ projectIndex ];
+            for ( let simulationIndex = 0; simulationIndex < project.simulations.length; simulationIndex++ ) {
+              const simulation = project.simulations[ simulationIndex ];
+              const repoDir = `../${simulation.name}`;
+
+              // Get master
+              await gitCheckout( simulation.name, 'master' );
+              await gitPull( simulation.name );
+
+              // Build screenshots
+              await execute( 'grunt', [ `--brands=${options.brands || 'phet'}`, `--repo=${simulation.name}`, 'build-images' ], chipperDir );
+
+              // Copy into the document root
+              let brands;
+              if ( options.brands ) {
+                if ( options.brands.length ) {
+                  brands = options.brands;
+                }
+                else {
+                  brands = options.brands.split( ',' );
+                }
+              }
+              else {
+                brands = [ 'phet' ];
+              }
+              for ( let brandIndex = 0; brandIndex < brands.length; brandIndex++ ) {
+                const brand = brands[ brandIndex ];
+                if ( brand !== 'phet' ) {
+                  reject( `Image deploy not implemented for brand: ${brand}` );
+                }
+                else {
+                  const sourceDir = `${repoDir}/build/${brand}/`;
+                  const targetDir = `${constants.HTML_SIMS_DIRECTORY}${simulation.name}/${project.version.string}/`;
+                  const files = fs.readdirSync( sourceDir );
+                  for ( let fileIndex = 0; fileIndex < files.length; fileIndex++ ) {
+                    const file = files[ fileIndex ];
+                    if ( file.endsWith( 'png' ) ) {
+                      console.log( `copying file ${file}` );
+                      await execute( 'cp', [ `${sourceDir}${file}`, `${targetDir}${file}` ], '.' );
+                    }
+                  }
+
+                  console.log( `Done copying files for ${simulation.name}` );
+                }
+              }
+            }
+          }
+
+          resolve();
+        }
+      } );
+    }
+    catch ( e ) {
+      reject( e );
+    }
+  } );
+
+};
+
+module.exports = deployImages;
