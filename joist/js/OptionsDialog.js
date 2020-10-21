8bb4ef16e859086ec0d8330307ce1a85a0b1d1a3
Luisav1 <52978048+Luisav1@users.noreply.github.com>
2020-10-21 12:49:13 -0600

    Add files via upload

---
+++ joist/js/OptionsDialog.js
@@ -0,0 +1,74 @@
+// Copyright 2014-2020, University of Colorado Boulder
+
+/**
+ * Shows an Options dialog that consists of sim-global options.
+ *
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+
+import inherit from '../../phet-core/js/inherit.js';
+import merge from '../../phet-core/js/merge.js';
+import PhetFont from '../../scenery-phet/js/PhetFont.js';
+import Text from '../../scenery/js/nodes/Text.js';
+import Dialog from '../../sun/js/Dialog.js';
+import Tandem from '../../tandem/js/Tandem.js';
+import joist from './joist.js';
+import joistStrings from './joistStrings.js';
+
+const optionsTitleString = joistStrings.options.title;
+
+/**
+ * @param {function(tandem:Tandem):Node} createContent - creates the dialog's content
+ * @param {Object} [options]
+ * @constructor
+ */
+function OptionsDialog( createContent, options ) {
+
+  options = merge( {
+
+    titleAlign: 'center',
+    bottomMargin: 26,
+    ySpacing: 26,
+
+    // phet-io
+    tandem: Tandem.REQUIRED,
+    phetioDynamicElement: true
+  }, options );
+
+  assert && assert( !options.title, 'OptionsDialog sets title' );
+  options.title = new Text( optionsTitleString, {
+    font: new PhetFont( 30 ),
+    maxWidth: 400, // determined empirically
+    tandem: options.tandem.createTandem( 'title' )
+  } );
+
+  const content = createContent( options.tandem.createTandem( 'content' ) );
+
+  Dialog.call( this, content, options );
+
+  // @private
+  this.disposeOptionsDialog = () => {
+    content.dispose();
+    options.title.dispose();
+  };
+}
+
+joist.register( 'OptionsDialog', OptionsDialog );
+
+inherit( Dialog, OptionsDialog, {
+
+  /**
+   * @public
+   * @override
+   */
+  dispose: function() {
+    this.disposeOptionsDialog();
+    Dialog.prototype.dispose.call( this );
+  }
+}, {
+  DEFAULT_FONT: new PhetFont( 20 ),
+  DEFAULT_SPACING: 14
+} );
+
+export default OptionsDialog;
---
+++ joist/js/joist-tests.js
@@ -0,0 +1,16 @@
+// Copyright 2019-2020, University of Colorado Boulder
+
+/**
+ * Unit tests for joist.
+ *
+ * @author Sam Reid (PhET Interactive Simulations)
+ * @author Chris Klusendorf (PhET Interactive Simulations)
+ * @author Michael Kauzmann (PhET Interactive Simulations)
+ */
+
+import './selectScreensTests.js';
+import './TemporalCounterTests.js';
+import qunitStart from '../../chipper/js/sim-tests/qunitStart.js';
+
+// Since our tests are loaded asynchronously, we must direct QUnit to begin the tests
+qunitStart();
