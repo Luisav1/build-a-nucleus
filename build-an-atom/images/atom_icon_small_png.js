41b5a5c725b8531b46b698a6a9764d77f007856c
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2021-01-03 20:17:06 -0700

    Delete empty build-an-atom folder and other commits not committed from moving build-an-atom to top level build-a-nucleus

--- build-an-atom/images/atom_icon_small_png.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-import simLauncher from '../../joist/js/simLauncher.js';
-const image = new Image();
-const unlock = simLauncher.createLock( image );
-image.onload = unlock;
-image.src = 'https://energyeducation.ca/simulations/build-a-nucleus/images/atom_icon_small.png';
-export default image;
